26ef89f2ca1de79f0e870718dc7b78e8f7f6674d
Alban BOITIER <aboitier@e2r10p11.42.fr>
2020-03-10 21:57:29 +0100

    scan implemented

--- asm/src/main.c
+++ asm/src/main.c
@@ -6,7 +6,7 @@
 /*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/04 18:59:55 by aboitier          #+#    #+#             */
-/*   Updated: 2020/03/04 22:29:34 by aboitier         ###   ########.fr       */
+/*   Updated: 2020/03/10 21:51:08 by aboitier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,18 @@ int		safe_open(char *av)
 {
 	int fd; 
 
-	if ((fd = open(&av[1], 0, O_RDONLY)) == 0)
+	errno = 0;
+	if ((fd = open(av, O_RDONLY)) == -1)
 		return (FALSE);
+	printf("FD\t\t\tOK\n");
 	return (fd);
 }
 
-int		check_valid_argv(char *av, t_asm *env)
+/*
+**	We only accept .s files -- ε (empty string) as filename not accepted.
+*/
+
+int		check_valid_argv(t_asm *env, char *av)
 {
 	int i;
 
@@ -29,16 +35,22 @@ int		check_valid_argv(char *av, t_asm *env)
 	while (av[i])
 		i++;
 	if (av[i - 2] != '.' || av[i - 1] != 's' || !av[i - 3])
+	{
+		ft_putstr(FILE_ERROR);
+		return (FALSE);
+	}
+	if ((env->fd = safe_open(av)) == FALSE)
 		return (FALSE);
 	av[i - 2] = '\0';
 	env->file_name = av;
+	printf("CHECK_ARGV\t\tOK\n");
 	return (TRUE);
 }
 
 int		init(t_asm *env, char *av)
 {
-	if ((check_valid_argv(av, env) == FALSE \
-		|| (env->fd = safe_open(av)) == FALSE))
+	if ((check_valid_argv(env, av) == FALSE \
+		|| scan(env) == FALSE))
 		return (FALSE);
 	return (TRUE);
 }
@@ -47,14 +59,9 @@ int		main(int ac, char **av)
 {
 	t_asm	env;
 
-//	ft_bzero(env, sizeof(t_asm));
+	ft_bzero(&env, sizeof(t_asm));
 	if (ac != 2 || init(&env, av[1]) == FALSE)
-	{
-		printf("Error in input file\n");
-//		ft_putstr("Please specify an input file ending with .s");
 		return (FALSE);
-	}
 	printf("file_name = %s\n", env.file_name);
-	printf("%d\n", op_tab[0].opcode);
 	return (0);
 }
---
+++ asm/src/scan.c
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   scan.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aboitier <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/10 15:01:18 by aboitier          #+#    #+#             */
+/*   Updated: 2020/03/10 21:52:26 by aboitier         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/asm.h"
+
+int			set_buffer(t_scan *scan)
+{
+	ft_bzero(scan, sizeof(t_scan));
+	if (!(scan->buffer = (char *)ft_memalloc(sizeof(char) * BASE_MALLOC + 1)))
+		return (FALSE);
+	scan->reload = 1;
+	return (TRUE);
+}
+
+int			increase_buffer(t_scan *scan)
+{
+	char	*new_buffer;
+
+	new_buffer = NULL;
+	if (!(new_buffer = (char *)ft_memalloc(sizeof(char) 
+					* BASE_MALLOC * ++scan->reload + 1)))
+		return (FALSE);
+	new_buffer = ft_strcpy(new_buffer, scan->buffer);
+	free(scan->buffer);
+	scan->buffer = new_buffer;
+	return (TRUE);
+}
+
+int			copy_fr_index(t_scan *scan, char *src, long index)
+{
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		scan->buffer[index] = src[i];
+		index++;
+		i++;
+	}
+	scan->buffer[index] = '\0';
+	return (index);
+}
+
+//int			check_first_lines(t_scan scan)
+//{
+//	int i;
+//
+//	i = 0;
+//	if (scan.buffer[0] == '\0')
+//		return (FALSE);
+//	while (scan.buffer[i] && ft_isspace(scan.buffer[i]) != 0)
+//		i++;
+//	if (!scan.buffer || scan.buffer[i++] != '.')
+//		return (FALSE);	
+//	if (!scan.buffer || (scan.buffer[i] != 'n' && scan.buffer[i] != 'c'))
+//		return (FALSE);	
+//	return (TRUE);
+//}
+
+int			scan(t_asm *env)
+{
+	int		ret;
+	char	buff[BUFF_SIZE + 1];
+	t_scan	scan;
+
+	if (set_buffer(&scan) == FALSE)
+		return (FALSE);
+	while ((ret = read(env->fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		scan.buff_len += ret;
+		if (scan.buff_len >= (long)BASE_MALLOC * scan.reload)
+			if (increase_buffer(&scan) == FALSE)
+				return (FALSE);
+		scan.index = copy_fr_index(&scan, buff, scan.index);
+//		if (scan.reload == 3)
+//			if (check_first_lines(scan) == FALSE)
+//				return (FALSE);
+	}
+	env->scan = scan;
+	printf("SCAN\t\t\tOK\n");
+	return (TRUE);
+}
