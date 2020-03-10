c720af47388e80badbdcf371dc835263e9be667d
Paul JULLIEN <paul.jullien.code@gmail.com>
2020-03-10 18:27:42 +0100

    Ajout du main updated

--- virtual_machine/srcs/test.c
+++ virtual_machine/srcs/test.c
@@ -6,7 +6,7 @@
 /*   By: pauljull <pauljull@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 16:31:14 by pauljull          #+#    #+#             */
-/*   Updated: 2020/03/10 08:55:07 by pauljull         ###   ########.fr       */
+/*   Updated: 2020/03/10 18:06:57 by pauljull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,18 +24,19 @@ void	ft_move_pc(t_process *process, int value)
 	process->pc = (process->pc + value) % MEM_SIZE;
 }
 
-int	main()
+int	main(int ac, char **av)
 {
 	t_vm			vm;
 	size_t			cycles;
 	t_process		*tab[CYCLE_WAIT_MAX];
 
-	ft_create_vm(&vm);
+	ft_parse(ac - 1, av + 1, vm.vm);
+	ft_dump(vm.vm);
 	bzero(tab, sizeof(tab));
 	if (!ft_create_processus_list(2, tab, &vm))
 		return (ft_error());
 	cycles = 0;
-	while (cycles < CYCLE_MAX + 1)
+	while (cycles < CYCLE_TO_DIE)
 	{
 		printf("cycle %zu\n", cycles);
 		ft_exec_cycle(&vm, tab, cycles);
