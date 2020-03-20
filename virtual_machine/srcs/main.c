cbbe1f1869c931603274be23a89022144ab86ad0
Paul JULLIEN <paul.jullien.code@gmail.com>
2020-03-20 16:42:08 +0100

    Ancien main

--- virtual_machine/srcs/main.c
+++ virtual_machine/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: paul <paul@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 16:31:14 by pauljull          #+#    #+#             */
-/*   Updated: 2020/03/19 09:41:15 by paul             ###   ########.fr       */
+/*   Updated: 2020/03/20 16:37:04 by paul             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,25 @@
 #include "../includes/prototypes.h"
 #include "../../libft/includes/libft.h"
 
-void	ft_init(t_vm *vm, size_t *cycles, t_process *tab[CYCLE_WAIT_MAX])
+void	ft_init(t_vm *vm, t_process *tab[CYCLE_WAIT_MAX])
 {
 	ft_bzero(vm, sizeof(vm[0]));
 	ft_bzero(tab, sizeof(CYCLE_WAIT_MAX * sizeof(t_process *)));
-	cycles[0] = 0;
+	vm->cycles_to_die = CYCLE_TO_DIE;
+	vm->nb_max_process = 64;
 }
 
 int		main(int ac, char **av)
 {
 	t_vm			vm;
-	size_t			cycles;
 	t_process		*tab[CYCLE_WAIT_MAX];
 
-	ft_init(&vm, &cycles, tab);
+	ft_init(&vm, tab);
 	if (ft_parse(ac - 1, av + 1, &vm))
 		return (false);
+	if (!ft_create_processus_list(vm.nb_player, tab, &vm))
+		return (false);
+	ft_debug_processus_list(vm.process_list, vm.nb_max_process);
 	return (0);
 }
 
