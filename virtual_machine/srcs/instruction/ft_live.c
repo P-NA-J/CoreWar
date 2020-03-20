7e420b4ad2143aabbff4ce669fc1fcf9614f6c9a
danglass <danglass@MacBook-Air-de-admin.local>
2020-03-20 10:52:38 +0100

    ft_live

--- virtual_machine/srcs/instruction/ft_live.c
+++ virtual_machine/srcs/instruction/ft_live.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_live.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: paul <paul@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: danglass <danglass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 15:35:27 by pauljull          #+#    #+#             */
-/*   Updated: 2020/03/20 10:41:11 by paul             ###   ########.fr       */
+/*   Updated: 2020/03/20 10:52:11 by danglass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,8 @@ void	ft_live(t_process *process, t_vm *vm)
 	int			nb_player;
 	int			param;
 
-	nb_player = vm->nb_player * (-&);
+	nb_player = vm->nb_player * (-1);
 	param = vm->param[0][0];
-	if (param < 0 && param >= )
+	if (param < 0 && param >= nb_player)
+		ft_write_player_alive();
 }
