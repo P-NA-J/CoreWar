b976486f36c6f769779078790ce996bea2a21a1b
danglass <danglass@MacBook-Air-de-admin.local>
2020-03-23 18:21:08 +0100

    ldi

--- virtual_machine/srcs/instruction/ft_ldi.c
+++ virtual_machine/srcs/instruction/ft_ldi.c
@@ -3,18 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_ldi.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pauljull <pauljull@student.42.fr>          +#+  +:+       +#+        */
+/*   By: danglass <danglass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 15:35:06 by pauljull          #+#    #+#             */
-/*   Updated: 2020/03/12 15:04:38 by pauljull         ###   ########.fr       */
+/*   Updated: 2020/03/23 18:20:18 by danglass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/tab.h"
 #include "../../includes/struct.h"
 
+int		ft_value_from_address(int pc, int indirect, t_vm *vm)
+{
+	int param;
+	int pos;
+
+	pos = pc + (indirect % IDX_MOD);
+	param = ft_convert_to_int(vm->vm + pos);
+	return (param);
+}
+
 void	ft_ldi(t_process *process, t_vm *vm)
 {
-	(void)process;
-	(void)vm;
+	int param_1;
+	int param_2;
+	int	value;
+
+	param_1 = vm->param[0][0];
+	param_2 = vm->param[1][0];
+	if (IND_BIT == vm->param[0][1])
+		param_1 = ft_value_from_address(process->pc, vm->param[0][0], vm);
+	else if (REG_BIT == vm->param[0][1])
+		param_1 = process->registre[vm->param[0][0] - 1];
+	if (REG_BIT == vm->param[1][1])
+		param_2 = process->registre[vm->param[1][0] - 1];
+	value = param_1 + param_2;
+	value = ft_value_from_address(process->pc, value, vm);
 }
