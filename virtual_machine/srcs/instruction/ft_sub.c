b31ae169a69de5b9dc14593f36ee3e641422f600
Paul JULLIEN <paul.jullien.code@gmail.com>
2020-03-23 17:36:56 +0100

    Modification des index dans ft_add et ft_sub

--- virtual_machine/srcs/instruction/ft_sub.c
+++ virtual_machine/srcs/instruction/ft_sub.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_sub.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: danglass <danglass@student.42.fr>          +#+  +:+       +#+        */
+/*   By: paul <paul@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 15:37:39 by pauljull          #+#    #+#             */
-/*   Updated: 2020/03/23 17:30:33 by danglass         ###   ########.fr       */
+/*   Updated: 2020/03/23 17:36:13 by paul             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_sub(t_process *process, t_vm *vm)
 {
 	int	sub;
 
-	sub = process->registre[vm->param[0][0]];
-	sub -= process->registre[vm->param[1][0]];
-	process->registre[vm->param[2][0]] = sub;
+	sub = process->registre[vm->param[0][0] - 1];
+	sub -= process->registre[vm->param[1][0] - 1];
+	process->registre[vm->param[2][0] - 1] = sub;
 }
