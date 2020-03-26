af5af40bfcccd80007ad9753dd72a2aa867c3d9d
danglass <danglass@MacBook-Air-de-admin.local>
2020-03-26 17:39:04 +0100

    lld lldi

--- virtual_machine/srcs/instruction/ft_lldi.c
+++ virtual_machine/srcs/instruction/ft_lldi.c
@@ -6,14 +6,14 @@
 /*   By: danglass <danglass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/10 15:36:56 by pauljull          #+#    #+#             */
-/*   Updated: 2020/03/26 17:36:52 by danglass         ###   ########.fr       */
+/*   Updated: 2020/03/26 17:38:29 by danglass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/tab.h"
 #include "../../includes/struct.h"
 
-int		ft_lldi_param_recover_value(t_vm *vm, t_process *process, uint32_t tab[2])
+int		ft_lldi_param_recover_value(t_process *process, uint32_t tab[2])
 {
 	int	param;
 
@@ -32,9 +32,9 @@ void	ft_lldi(t_process *process, t_vm *vm)
 	int	param_3;
 	int	value;
 
-	param_1 = ft_lldi_param_recover_value(vm, process, vm->param[0]);
-	param_2 = ft_lldi_param_recover_value(vm, process, vm->param[1]);
-	param_3 = ft_lldi_param_recover_value(vm, process, vm->param[2]);
+	param_1 = ft_lldi_param_recover_value(process, vm->param[0]);
+	param_2 = ft_lldi_param_recover_value(process, vm->param[1]);
+	param_3 = ft_lldi_param_recover_value(process, vm->param[2]);
 	value = param_1 + param_2;
 	process->registre[param_3 - 1] = value;
 	process->carry = (process->carry == 1 ? 0 : 1);
