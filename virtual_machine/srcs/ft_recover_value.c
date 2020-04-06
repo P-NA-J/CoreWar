5dc5d1797ffba6603f131b8e53251e21aea8587d
Paul JULLIEN <paul.jullien.code@gmail.com>
2020-04-06 10:36:15 +0200

    Clean du fichier main.C et création d'un fichier features recover_value

---
+++ virtual_machine/srcs/ft_recover_value.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recover_value.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: paul <paul@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/06 10:30:43 by paul              #+#    #+#             */
+/*   Updated: 2020/04/06 10:34:57 by paul             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "../includes/struct.h"
+#include "../includes/op.h"
+
+int		ft_recover_value_ram(uint8_t vm[4], int	len)
+{
+	int	i;
+	int	res;
+
+	i = 0;
+	while (i < len)
+	{
+		res = vm[i];
+		if (i < len - 1)
+			res <<= 8;
+		i += 1;
+	}
+	return (res);
+}
+
+int		ft_recover_value_param(uint8_t vm[MEM_SIZE], uint32_t param[2], t_process *process)
+{
+	int parameter;
+
+	if (param[1] == IND_BIT)
+		parameter = ft_recover_value_ram(vm + process->pc + (param[1] % IDX_MOD), 4);
+	else if (param[1] == REG_BIT)
+		parameter = process->registre[param[0] - 1];
+	else
+		parameter = param[0];
+	return (parameter);
+}
--- virtual_machine/srcs/main.c
+++ virtual_machine/srcs/main.c
@@ -6,47 +6,14 @@
 /*   By: paul <paul@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 16:31:14 by pauljull          #+#    #+#             */
-/*   Updated: 2020/04/06 10:26:19 by paul             ###   ########.fr       */
+/*   Updated: 2020/04/06 10:31:30 by paul             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../libft/includes/struct.h"
-#include "../../libft/includes/prototypes.h"
-#include "../includes/op.h"
 #include "../includes/struct.h"
 #include "../includes/debug.h"
 #include "../includes/prototypes.h"
-#include <stdlib.h>
 #include <stdbool.h>
-
-int		ft_get_value_ram(uint8_t vm[4], int	len)
-{
-	int	i;
-	int	res;
-
-	i = 0;
-	while (i < len)
-	{
-		res = vm[i];
-		if (i < len - 1)
-			res <<= 8;
-		i += 1;
-	}
-	return (res);
-}
-
-int		ft_recover_value_param(uint8_t vm[MEM_SIZE], uint32_t param[2], t_process *process)
-{
-	int parameter;
-
-	if (param[1] == IND_BIT)
-		parameter = ft_get_value_ram(vm + process->pc + (param[1] % IDX_MOD), 4);
-	else if (param[1] == REG_BIT)
-		parameter = process->registre[param[0] - 1];
-	else
-		parameter = param[0];
-	return (parameter);
-}
 
 int		main(int ac, char **av)
 {
@@ -58,10 +25,10 @@ int		main(int ac, char **av)
 		return (false);
 	if (!ft_processus_player_initialisation(&vm))
 		return (false);
-/*	if (vm.opt.d[0] == true)
+	if (vm.opt.d[0] == true)
 		ft_loop_dumped(&vm, vm.tab);
 	else
 		ft_loop_std(&vm, vm.tab);
-*/	return (0);
+	return (0);
 }
 
